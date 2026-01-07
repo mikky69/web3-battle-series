91572e021480065c11eea5811c949f6012b2eedf
Mikailu Nadro <74379292+mikky69@users.noreply.github.com>
2026-01-07 20:19:55 +0100

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    loader: 'tsx',
+    include: /src\/.*\.[tj]sx?$/,
+    exclude: []
+  }
+})
