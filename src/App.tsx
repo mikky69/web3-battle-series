0a678b20e72878e2c3417d32be12f1c163b0c6bb
Mikailu Nadro <74379292+mikky69@users.noreply.github.com>
2026-01-07 20:36:30 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import Layout from './components/Layout.tsx';
-import HomeSection from './components/HomeSection.tsx';
-import NFTBeltsSection from './components/NFTBeltsSection.tsx';
+import Layout from './components/Layout';
+import HomeSection from './components/HomeSection';
+import NFTBeltsSection from './components/NFTBeltsSection';
 
 const App: React.FC = () => {
   const [currentView, setCurrentView] = useState<'home' | 'nfts'>('home');
-
+  
   return (
     <Layout onViewChange={setCurrentView} currentView={currentView}>
       {currentView === 'home' ? <HomeSection /> : <NFTBeltsSection />}
