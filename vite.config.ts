a09661434c862c3996114d64b3e3f66add0eea77
Mikailu Nadro <74379292+mikky69@users.noreply.github.com>
2026-01-07 20:39:44 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: 'tsx',
-    include: /src\/.*\.[tj]sx?$/,
-    exclude: []
-  }
 })
